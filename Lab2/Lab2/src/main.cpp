ab1e7b13a243dd3fd2923fef9acc199ae249ab96
John Danison <bucketfoot35@gmail.com>
2025-01-24 14:24:56 -0500

    Added Lab 2 Platformio Framework

---
+++ Lab2/Lab2/src/main.cpp
@@ -0,0 +1,26 @@
+/**
+ * Filename: main.cpp
+ * Author: John Danison
+ * Date: 1/24/2025
+ * 
+ * ECET 32900 Lab 2 - Interrupts Review
+ */
+
+/* Libraries */
+#include <Arduino.h>
+
+/* Global Variables */
+
+/* Global Constnats */
+
+/* Function Prototypes */
+
+/* Setup Function */
+void setup() {
+  
+}
+
+/* Main Loop */
+void loop() {
+  // put your main code here, to run repeatedly:
+}
